7391951e3c0202eb100a95072671808ca0bca511
Gracious Mashasha <gracious.mashasha@gmail.com>
2025-01-28 11:00:21 +0200

    spring init

---
+++ src/main/java/io/caniverse/investment/CaniverseInvestmentApplication.java
@@ -0,0 +1,13 @@
+package io.caniverse.investment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaniverseInvestmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaniverseInvestmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/caniverse/investment/CaniverseInvestmentApplicationTests.java
@@ -0,0 +1,13 @@
+package io.caniverse.investment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaniverseInvestmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
