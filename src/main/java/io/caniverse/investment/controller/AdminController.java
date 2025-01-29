91311f7d005975e4074118f860d8c93e644a641a
Gracious Mashasha <gracious.mashasha@gmail.com>
2025-01-29 15:17:10 +0200

    Admin investments

---
+++ src/main/java/io/caniverse/investment/controller/AdminController.java
@@ -0,0 +1,27 @@
+package io.caniverse.investment.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("admin")
+public class AdminController {
+
+    @GetMapping({"","investments"})
+    String investments(){
+        return "admin/investments";
+    }
+
+    @GetMapping("investments/{id}")
+    String investment(@PathVariable Long id){
+        return "admin/investment";
+    }
+
+    @GetMapping("withdrawals")
+    String withdrawals(){
+        return "admin/withdrawals";
+    }
+
+}
