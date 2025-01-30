4b552dac9cbbdec66da1a4f4e810872a1f82e704
Gracious Mashasha <gracious.mashasha@gmail.com>
2025-01-30 12:22:03 +0200

    Auth templates

---
+++ src/main/java/io/caniverse/investment/controller/AuthController.java
@@ -0,0 +1,19 @@
+package io.caniverse.investment.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AuthController {
+
+    @GetMapping("login")
+    String login(){
+        return "login";
+    }
+
+    @GetMapping("register")
+    String register(){
+        return "register";
+    }
+}
