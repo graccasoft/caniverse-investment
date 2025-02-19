544391b83032bd79f78f80d8f8cb1992994348fa
Gracious Mashasha <gracious.mashasha@gmail.com>
2025-02-19 18:04:22 +0200

    Optionals for null safety

--- src/main/java/io/caniverse/investment/repository/WithdrawalRepository.java
+++ src/main/java/io/caniverse/investment/repository/WithdrawalRepository.java
@@ -8,17 +8,18 @@ import org.springframework.data.repository.query.Param;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
 
 public interface WithdrawalRepository extends JpaRepository<Withdrawal, Long> {
     List<Withdrawal> findAllByInvestorInvestment_Investor(Investor investor);
     List<Withdrawal> findAllByInvestorInvestment_Investment(Investment investment);
 
     @Query("SELECT SUM(w.amount) FROM Withdrawal w WHERE w.investorInvestment.investor = :investor AND w.status = :status")
-    BigDecimal getTotalByInvestorAndStatus(@Param("investor") Investor investor, @Param("status") TransactionStatus status);
+    Optional<BigDecimal> getTotalByInvestorAndStatus(@Param("investor") Investor investor, @Param("status") TransactionStatus status);
 
     @Query("SELECT SUM(w.amount) FROM Withdrawal w WHERE w.status = :status")
-    BigDecimal getTotalByStatus(@Param("status") TransactionStatus status);
+    Optional<BigDecimal> getTotalByStatus(@Param("status") TransactionStatus status);
 
     @Query("SELECT SUM(w.amount) FROM Withdrawal w WHERE w.investorInvestment = :investment")
-    BigDecimal getTotalByInvestment(@Param("investment") InvestorInvestment investor);
+    Optional<BigDecimal> getTotalByInvestment(@Param("investment") InvestorInvestment investor);
 }
